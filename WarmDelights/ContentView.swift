0df63f8bd604ccc9b10f298ebb30917e39189503
kowshimaniCONSTRUCTN <123066680+kowshimaniCONSTRUCTN@users.noreply.github.com>
2024-04-17 02:41:16 +0530

    1st push

---
+++ WarmDelights/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WarmDelights
+//
+//  Created by KowshiMani K on 17/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, WarmDelights!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WarmDelights/WarmDelightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmDelightsApp.swift
+//  WarmDelights
+//
+//  Created by KowshiMani K on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmDelightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
